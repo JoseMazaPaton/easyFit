67a5048def08840607492aed4e6382fb8661ac67
Jose <jose.maza@hotmail.es>
2025-03-20 17:27:38 +0100

    feat: startin project

---
+++ back/easyfit/src/main/java/easyfit/EasyfitApplication.java
@@ -0,0 +1,13 @@
+package easyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfitApplication.class, args);
+	}
+
+}
---
+++ back/easyfit/src/test/java/easyfit/EasyfitApplicationTests.java
@@ -0,0 +1,13 @@
+package easyfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
