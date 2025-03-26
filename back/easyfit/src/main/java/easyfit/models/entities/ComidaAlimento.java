dc660322264efc83243fc778411a458817d8f34e
Pablo <pabloprietolopez94@outlook.es>
2025-03-26 20:34:40 +0100

    feat(backend): Correcion errores en la entidad ComidaAlimento se le añade un id

--- back/easyfit/src/main/java/easyfit/models/entities/ComidaAlimento.java
+++ back/easyfit/src/main/java/easyfit/models/entities/ComidaAlimento.java
@@ -1,6 +1,11 @@
 package easyfit.models.entities;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -16,17 +21,24 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @Table(name="proyecto_con_empleados")
-public class ComidaAlimento {
+public class ComidaAlimento implements Serializable{
 
-    @Id
-    @ManyToOne
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_comida_alimento")
+    private int idComidaAlimento;
+
+    private double cantidad;
+    
+    @ManyToOne(optional = false)
     @JoinColumn(name = "id_comida")
     private Comida comida;
 
-    @Id
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "id_alimento")
     private Alimento alimento;
 
-    private double cantidad;
+
 }
