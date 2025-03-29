6eeffbfa2f772dd1ba601b7c788a335e3163e485
Pablo <pabloprietolopez94@outlook.es>
2025-03-29 12:18:13 +0100

    feat(back-end): Creacion estructura controladores + AuthController

---
+++ back/easyfit/src/main/java/easyfit/restcontroller/AuthController.java
@@ -0,0 +1,44 @@
+package easyfit.restcontroller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import easyfit.models.entities.Usuario;
+import easyfit.services.IUsuarioService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/auth")
+public class AuthController {
+
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	@PostMapping("/login")
+	public Usuario login() {
+		return null;
+	}
+	
+	@PostMapping("/logout")
+	public Usuario logout() {
+		return null;
+	}
+	
+	@PostMapping("/registro")
+	public Usuario registro() {
+		return null;
+	}
+	
+	@GetMapping("/me")
+	public Usuario obtenerDatosUsuario() {
+		return null;
+	}
+}
