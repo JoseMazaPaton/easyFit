9ce72296178effd20107526d1f466130d4123232
Pablo <pabloprietolopez94@outlook.es>
2025-04-09 10:27:52 +0200

    feat(frontend): parte usuario creada

--- front/easyfit/src/app/user/layouts/cliente-layout/cliente-layout.component.ts
+++ front/easyfit/src/app/user/layouts/cliente-layout/cliente-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../components/navbar/navbar.component";
+import { FooterComponent } from "../../../components/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-layout',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './cliente-layout.component.html',
   styleUrl: './cliente-layout.component.css'
 })
--- front/easyfit/src/app/user/user.routes.ts
+++ front/easyfit/src/app/user/user.routes.ts
@@ -1,5 +1,53 @@
 import { Routes } from '@angular/router';
 
 export const userRoutes: Routes = [
-
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/cliente-layout/cliente-layout.component').then(
+        m => m.ClienteLayoutComponent
+      ),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard-user/dashboard-user.component').then(
+            m => m.DashboardUserComponent
+          ),
+      },
+      {
+        path: 'diario',
+        loadComponent: () =>
+          import('./pages/diario-user/diario-user.component').then(
+            m => m.DiarioUserComponent
+          ),
+      },
+      {
+        path: 'alimentos',
+        loadComponent: () =>
+          import('./pages/alimentos-user/alimentos-user.component').then(
+            m => m.AlimentosUserComponent
+          ),
+      },
+      {
+        path: 'objetivos',
+        loadComponent: () =>
+          import('./pages/objetivos-user/objetivos-user.component').then(
+            m => m.ObjetivosUserComponent
+          ),
+      },
+      {
+        path: 'perfil',
+        loadComponent: () =>
+          import('./pages/perfil-user/perfil-user.component').then(
+            m => m.PerfilUserComponent
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
 ];
